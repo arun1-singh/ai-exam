45e9443a1e4e106b6f6a94a3b00571b01f5540bc
arun1-singh <arunsingh17683@gmail.com>
2025-02-26 18:12:52 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,10 +2,12 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 // import { helloWorld,CreateNewUser, GenerateNotes, testFunction  } from "../../../inngest/functions";
 import { CreateNewUser, GenerateNotes, GenerateStudyTypeContent, helloWorld } from "@/inngest/functions";
+export const runtime = 'edge';
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming:'allow',
   functions: [
     /* your functions will be passed here later! */
     helloWorld,
